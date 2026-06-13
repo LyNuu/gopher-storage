38e8917c828294e80ffabf9f8bc632a7139d81f0
LyyNu <levsz100@gmail.com>
2026-06-13 16:54:28 +0300

    feat: add docker-compose & create users table

---
+++ auth-service/migrations/20260613130921_create_users_table.sql
@@ -0,0 +1,21 @@
+-- +goose Up
+-- +goose StatementBegin
+-- +goose StatementEnd
+create type user_role as enum ('user', 'admin');
+
+create table users
+(
+    id            uuid primary key      default gen_random_uuid(),
+    email         varchar(255) not null unique,
+    password_hash varchar(255) not null,
+    role          user_role    not null default 'user',
+    created_at    TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
+    updated_at    TIMESTAMPTZ  NOT NULL DEFAULT NOW()
+);
+
+CREATE INDEX idx_users_email ON users (email);
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS users;
+DROP TYPE IF EXISTS user_role;
+-- +goose StatementEnd
